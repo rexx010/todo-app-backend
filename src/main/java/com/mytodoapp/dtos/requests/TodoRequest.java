d354055357b48ee0e26b66088e6e39e5ea39def4
rexx010 <grexx200@gmail.com>
2025-09-16 16:36:21 +0100

    Add validation annotations to TodoRequest fields for title and description

--- src/main/java/com/mytodoapp/dtos/requests/TodoRequest.java
+++ src/main/java/com/mytodoapp/dtos/requests/TodoRequest.java
@@ -1,12 +1,15 @@
 package com.mytodoapp.dtos.requests;
 
 import com.mytodoapp.data.models.Status;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class TodoRequest {
+    @NotBlank(message = "Task title is required")
     private String title;
+    @NotBlank(message = "Task description is required")
     private String description;
 }
