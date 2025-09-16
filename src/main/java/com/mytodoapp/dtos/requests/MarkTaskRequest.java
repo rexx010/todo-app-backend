474380d86ae06c722112f3131107c687cbf52071
rexx010 <grexx200@gmail.com>
2025-09-16 16:35:53 +0100

    Add validation to MarkTaskRequest for taskId field

--- src/main/java/com/mytodoapp/dtos/requests/MarkTaskRequest.java
+++ src/main/java/com/mytodoapp/dtos/requests/MarkTaskRequest.java
@@ -1,8 +1,10 @@
 package com.mytodoapp.dtos.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class MarkTaskRequest {
+    @NotBlank(message = "Task id is required")
     private String taskId;
 }
