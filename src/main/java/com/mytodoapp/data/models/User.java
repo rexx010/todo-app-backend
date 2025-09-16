ad72a1f860f01dd256963c94d26f2fb8e16a3947
rexx010 <grexx200@gmail.com>
2025-09-16 16:22:17 +0100

    Add User model with fields and MongoDB annotations

---
+++ src/main/java/com/mytodoapp/data/models/User.java
@@ -0,0 +1,15 @@
+package com.mytodoapp.data.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+public class User {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+}
