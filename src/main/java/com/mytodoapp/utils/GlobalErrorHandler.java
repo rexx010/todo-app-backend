60b36b37d582804641cb11d4d51c0fc7df86a515
rexx010 <grexx200@gmail.com>
2025-11-02 00:10:30 +0100

    Annotate `GlobalErrorHandler` with `@ControllerAdvice` to enable centralized exception handling.

--- src/main/java/com/mytodoapp/utils/GlobalErrorHandler.java
+++ src/main/java/com/mytodoapp/utils/GlobalErrorHandler.java
@@ -4,12 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+@ControllerAdvice
 public class GlobalErrorHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, String>> handleException(RuntimeException ex) {
