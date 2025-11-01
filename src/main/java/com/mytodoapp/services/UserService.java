9722eba09d2753e7f8b57d75065f720bc051c9f2
rexx010 <grexx200@gmail.com>
2025-11-01 10:39:21 +0100

    Fix typo: Rename `UserReponse` to `UserResponse` in `UserService` interface.

--- src/main/java/com/mytodoapp/services/UserService.java
+++ src/main/java/com/mytodoapp/services/UserService.java
@@ -1,14 +1,14 @@
 package com.mytodoapp.services;
 
 import com.mytodoapp.data.models.User;
-import com.mytodoapp.dtos.reponses.UserReponse;
+import com.mytodoapp.dtos.reponses.UserResponse;
 import com.mytodoapp.dtos.requests.LoginRequest;
 import com.mytodoapp.dtos.requests.UserRequest;
 
 import java.util.Optional;
 
 public interface UserService {
-    UserReponse registeredUser(UserRequest userRequest);
+    UserResponse registeredUser(UserRequest userRequest);
     Optional<User> login(LoginRequest loginRequest);
     Optional<User> getUserById(String id);
     void deleteById(String id);
